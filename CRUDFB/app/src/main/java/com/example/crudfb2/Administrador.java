062a1edf0d42c9f20a78f4f48e54af6a74ad1ece
Fernandoelcrack <yayferyayfer9.com>
2023-04-19 23:33:08 -0600

    first try

---
+++ CRUDFB/app/src/main/java/com/example/crudfb2/Administrador.java
@@ -0,0 +1,93 @@
+package com.example.crudfb2;
+
+public class Administrador{
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private int edad;
+    private String fecha;
+    private String pass;
+
+    public Administrador() {
+    }
+
+    public Administrador(int id, String nombre, String apellido, String correo, int edad, String fecha, String pass) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.edad = edad;
+        this.fecha = fecha;
+        this.pass = pass;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    @Override
+    public String toString() {
+        return "Administrador{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", correo='" + correo + '\'' +
+                ", edad=" + edad +
+                ", fecha='" + fecha + '\'' +
+                ", pass='" + pass + '\'' +
+                '}';
+    }
+}
---
+++ CRUDFB/app/src/main/java/com/example/crudfb2/MainActivity.java
@@ -0,0 +1,344 @@
+package com.example.crudfb2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtid, txtnom, txtapp, txtcorreo, txtedad, txtfecha, txtpass;
+    private Button btnbus, btnmod, btnreg, btneli;
+    private ListView lvDatos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtid   = (EditText) findViewById(R.id.txtid);
+        txtnom  = (EditText) findViewById(R.id.txtnom);
+        txtapp = (EditText) findViewById(R.id.txtapp);
+        txtcorreo = (EditText) findViewById(R.id.txtcorreo);
+        txtedad = (EditText) findViewById(R.id.txtedad);
+        txtfecha = (EditText) findViewById(R.id.txtfecha);
+        txtpass = (EditText) findViewById(R.id.txtpass);
+        btnbus  = (Button)   findViewById(R.id.btnbus);
+        btnmod  = (Button)   findViewById(R.id.btnmod);
+        btnreg  = (Button)   findViewById(R.id.btnreg);
+        btneli  = (Button)   findViewById(R.id.btneli);
+        lvDatos = (ListView) findViewById(R.id.lvDatos);
+
+        //botonBuscar();
+        //botonModificar();
+        botonRegistrar();
+        //botonEliminar();
+        //listarAdministradores();
+    }
+
+    /*private void botonBuscar(){
+        btnbus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtid.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Digite el ID del luchador a buscar", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    int id = Integer.parseInt(txtid.getText().toString());
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference dbref = db.getReference(Administrador.class.getSimpleName());
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            String aux = Integer.toString(id);
+                            boolean res = false;
+                            for(DataSnapshot x : snapshot.getChildren()){
+                                if(aux.equalsIgnoreCase(x.child("id").getValue().toString())){
+                                    res = true;
+                                    ocultarTeclado();
+                                    txtnom.setText(x.child("nombre").getValue().toString());
+                                    break;
+                                }
+                            }
+                            if(res == false){
+                                Toast.makeText(MainActivity.this, "ID ("+aux+") No Encontrado", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }//fin if
+            }
+        });
+        
+    }//botonbuscar */
+
+    /* private void botonModificar(){
+        btnmod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtid.getText().toString().trim().isEmpty() || txtnom.getText().toString().trim().isEmpty()){
+                    ocultarTeclado();
+                    Toast.makeText(MainActivity.this, "Complete los campos faltantes para actualizar", Toast.LENGTH_SHORT).show();
+                }else{
+                    int id = Integer.parseInt(txtid.getText().toString());
+                    String nom = txtnom.getText().toString();
+
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference dbref = db.getReference(Administrador.class.getSimpleName());
+
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            String aux = Integer.toString(id);
+                            boolean res = false;
+                            for(DataSnapshot x : snapshot.getChildren()){
+                                if(x.child("id").getValue().toString().equalsIgnoreCase(aux)){
+                                    res = true;
+                                    ocultarTeclado();
+                                    x.getRef().child("nombre").setValue(nom);
+                                    txtid.setText("");
+                                    txtnom.setText("");
+                                    ocultarTeclado();
+                                    listarAdministradores();
+                                    break;
+                                }
+                            }
+
+                            if(res == false){
+
+                                ocultarTeclado();
+                                Toast.makeText(MainActivity.this, "ID ("+aux+") de Administrador Modificado", Toast.LENGTH_SHORT).show();
+
+                                txtid.setText("");
+                                txtnom.setText("");
+                            }
+
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+
+    }//finalmodificar */
+
+    private void botonRegistrar(){
+        btnreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(txtid.getText().toString().trim().isEmpty() || txtnom.getText().toString().trim().isEmpty() || txtapp.getText().toString().trim().isEmpty() || txtcorreo.getText().toString().trim().isEmpty() || txtedad.getText().toString().trim().isEmpty() || txtfecha.getText().toString().trim().isEmpty() || txtpass.getText().toString().trim().isEmpty()){
+                    ocultarTeclado();
+                    Toast.makeText(MainActivity.this, "Complete los campos faltantes", Toast.LENGTH_SHORT).show();
+                }else{
+                    int id = Integer.parseInt(txtid.getText().toString());
+                    String nom = txtnom.getText().toString();
+                    String app = txtapp.getText().toString();
+                    String correo = txtcorreo.getText().toString();
+                    int edad = Integer.parseInt(txtedad.getText().toString());
+                    String fecha = txtfecha.getText().toString();
+                    String pass = txtpass.getText().toString();
+
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference dbref = db.getReference(Administrador.class.getSimpleName());
+
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            String aux = Integer.toString(id);
+                            boolean res = false;
+                            for(DataSnapshot x : snapshot.getChildren()){
+                                if(x.child("id").getValue().toString().equalsIgnoreCase(aux)){
+                                    res = true;
+                                    ocultarTeclado();
+                                    Toast.makeText(MainActivity.this, "ERROR, El ID ("+aux+") YA EXISTE", Toast.LENGTH_SHORT).show();
+                                    ocultarTeclado();
+                                    break;
+                                }
+                            }
+
+                            if(res == false){
+
+                                Administrador adm = new Administrador(id, nom, app, correo, edad, fecha, pass);
+                                dbref.push().setValue(adm);
+                                ocultarTeclado();
+                                Toast.makeText(MainActivity.this, "Administrador Registrado", Toast.LENGTH_SHORT).show();
+
+                                txtid.setText("");
+                                txtnom.setText("");
+                                txtapp.setText("");
+                                txtcorreo.setText("");
+                                txtedad.setText("");
+                                txtfecha.setText("");
+                                txtpass.setText("");
+                            }
+
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+            }
+        });
+    }//findelbotonregistrar
+
+    /* private void listarAdministradores(){
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference dbref = db.getReference(Administrador.class.getSimpleName());
+        ArrayList<Administrador> lisadm = new ArrayList<Administrador>();
+        ArrayAdapter <Administrador> ada = new ArrayAdapter<Administrador>(MainActivity.this, android.R.layout.simple_list_item_1, lisadm);
+        lvDatos.setAdapter(ada);
+
+        dbref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                Administrador adm = snapshot.getValue(Administrador.class);
+                lisadm.add(adm);
+                ada.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                ada.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        lvDatos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Administrador adm = lisadm.get(position);
+                AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
+                a.setCancelable(true);
+                a.setTitle("Administrador seleccionado");
+                String msg = "ID : " + adm.getId() +"\n\n";
+                msg += "NOMBRE : " +adm.getNombre();
+
+                a.setMessage(msg);
+                a.show();
+            }
+        });
+
+    }//final metodo */
+
+
+   /* private void botonEliminar(){
+        btneli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(txtid.getText().toString().trim().isEmpty()){
+                    Toast.makeText(MainActivity.this, "Digite el ID del luchador a buscar", Toast.LENGTH_SHORT).show();
+
+                }else{
+                    int id = Integer.parseInt(txtid.getText().toString());
+                    FirebaseDatabase db = FirebaseDatabase.getInstance();
+                    DatabaseReference dbref = db.getReference(Administrador.class.getSimpleName());
+                    dbref.addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            String aux = Integer.toString(id);
+                            final boolean[] res = {false};
+                            for(DataSnapshot x : snapshot.getChildren()){
+                                if(aux.equalsIgnoreCase(x.child("id").getValue().toString())){
+                                    AlertDialog.Builder a = new AlertDialog.Builder(MainActivity.this);
+                                    a.setTitle("Pregunta");
+                                    a.setMessage("Está seguro de querer borrar el registro?");
+                                    a.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                        }
+                                    });
+                                    a.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            res[0] = true;
+                                            ocultarTeclado();
+                                            x.getRef().removeValue();
+                                            listarAdministradores();
+
+                                        }
+                                    });
+                                    a.show();
+                                    break;
+
+                                }
+                            }
+                            if(res[0] == false){
+                                Toast.makeText(MainActivity.this, "ID ("+aux+") Imposible de Eliminar", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }//fin if
+            }
+        });
+    }//fineliminar */
+
+    private void ocultarTeclado(){
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    } // Cierra el método ocultarTeclado.
+
+
+}
